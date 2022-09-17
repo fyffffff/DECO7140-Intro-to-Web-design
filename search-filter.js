74a3202ea7348061910ec2efe10740304e857e53
Yifan Feng <94620786+fyffffff@users.noreply.github.com>
2022-09-17 23:28:57 +1000

    Delete search-filter.js

--- search-filter.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("#ProductSearchFilter").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $(".feature-product-item .product-intro").filter(function() {
-      $(this).parent().parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
